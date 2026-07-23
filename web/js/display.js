d5a1451afb1735436f946316822f25b5da55357e
antoineg44 <67955066+antoineg44@users.noreply.github.com>
2026-07-23 16:54:01 +0200

    icone changée

--- web/js/display.js
+++ web/js/display.js
@@ -74,7 +74,7 @@ function getDisplayHTMLButtons(item, el, infos, index) {
 
   item.innerHTML += `<div class="actions">`;
   if (infos.buttons.play) {
-    item.innerHTML += `<button class="${infoClassName}" type="button" data-action="info">ⓘ</button>`;
+    item.innerHTML += `<button class="${infoClassName}" type="button" data-action="info">i</button>`;
   }
   if(infos.buttons.delete) {
     item.innerHTML += `<button class="${deleteClassName}" type="button" data-action="delete">✕</button>`;
