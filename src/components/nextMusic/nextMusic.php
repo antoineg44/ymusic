292baced8e8d5c6d54fd65b64db76c10809e4c24
antoineg44 <antoine.godet@mailo.com>
2026-07-26 23:35:39 +0200

    suite

--- src/components/nextMusic/nextMusic.php
+++ src/components/nextMusic/nextMusic.php
@@ -1,5 +1,10 @@
 <?php
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
+
 // API principale: recherche, playlist, telechargement, metadonnees et routes artistes/albums.
 
 require '../../php/yt/YouTubeMusic.php';
