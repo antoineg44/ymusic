52de5dc324c230bbefcf7c6c1ba0fa0c94cc4b73
antoineg44 <67955066+antoineg44@users.noreply.github.com>
2026-07-29 14:33:25 +0200

    suite

--- src/components/interface/db_interface.js
+++ src/components/interface/db_interface.js
@@ -1,1 +1,72 @@
 
+function get_event_file(event)
+{
+    const location = event.source.location.href.split("/");
+    return location[location.length-1].substring(0,4);
+}
+
+async function sendResponse(source, messageId, url) {
+  const response = await fetch(get_url() + url, {
+    cache: 'no-store',
+  });
+
+  if (response.status === 401) {
+      window.postMessage({type: 'USER_LOGGED_OUT' }, '*');
+      return;
+    }
+
+    const dataText = await response.json();
+    if (!response.ok || !dataText.success) {
+      throw new Error(dataText.error || 'Error message');
+    }
+
+  source.postMessage({
+      response: dataText,
+      replyTo: messageId
+  }, '*');
+}
+
+
+async function db_listener(event)
+{
+  const data = event && event.data ? event.data : {};
+  const message = data && data.message ? data.message : {};
+
+  switch (get_event_file(event)) {
+    case "home":
+    // Envoyer la réponse en reply
+    await sendResponse(event.source, data.messageId, "pages/home/home.php?" + String(message));
+    return;
+
+    default:
+    break;
+  }
+
+  switch(message.action)
+  {
+    case 'check':
+      await sendResponse(event.source, data.messageId, "php/auth.php?action=" + message.action);
+      break;
+
+    case 'login': {
+      const response = await fetch(
+        get_url_from_base() + `php/auth.php?action=` + message.action,
+        {
+        method: "POST",
+        body: new URLSearchParams(message.body),
+        },
+      );
+      const dataText = await response.text();
+      event.source.postMessage({
+        response: dataText,
+        replyTo: data.messageId
+      }, '*');
+      break;
+    }
+
+    default:
+      console.log("event message action unknown : " + message.action);
+      break;
+  }
+
+}
