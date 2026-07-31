d7644b52577d0f691844b91f2afdedd4dc34765f
antoineg44 <antoine.godet@mailo.com>
2026-08-01 00:59:59 +0200

    suite

--- src/php/database/interface.php
+++ src/php/database/interface.php
@@ -1,9 +1,9 @@
 <?php
 
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
-
 // API principale: recherche, playlist, telechargement, metadonnees et routes artistes/albums.
 
 require '../yt/YouTubeMusic.php';
