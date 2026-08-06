32136571fa547185bc9da4443f501865248a515f
antoineg44 <67955066+antoineg44@users.noreply.github.com>
2026-08-06 10:52:06 +0200

    suite

--- src/python/main.py
+++ src/python/main.py
@@ -8,14 +8,14 @@ search_results = ytmusic.search("Les démons de minuit")
 
 print(search_results)
 
-print(search_results[0]['videoId'])
+#print(search_results[0]['videoId'])
 
-songs = ytmusic.get_watch_playlist(videoId=search_results[0]['videoId'])
+#songs = ytmusic.get_watch_playlist(videoId=search_results[0]['videoId'])
 
-print(songs["tracks"])
+#print(songs["tracks"])
 
 # Première suggestion
-next_video = songs["tracks"][1]  # tracks[0] est souvent la vidéo en cours
+#next_video = songs["tracks"][1]  # tracks[0] est souvent la vidéo en cours
 
-print(next_video["title"])
-print(next_video["videoId"])
+#print(next_video["title"])
+#print(next_video["videoId"])
