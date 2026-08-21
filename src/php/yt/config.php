8fd5890193282bc1f6a94daad79503c4092e4c17
antoineg44 <67955066+antoineg44@users.noreply.github.com>
2026-08-21 22:54:41 +0200

    config

--- src/php/yt/config.php
+++ src/php/yt/config.php
@@ -5,6 +5,6 @@
 
 return [
     // Adresse IPv6 entre crochets pour former une URL HTTP valide.
-    'remote_base_url' => getenv('YT_REMOTE_BASE_URL') ?: 'http://[2a02:8424:894c:a901:211:32ff:fe99:630b]',
+    'remote_base_url' => getenv('YT_REMOTE_BASE_URL') ?: 'http://[2a02:8424:894c:a901:211:32ff:fe99:630b]/nextcloud',
     'remote_endpoint' => getenv('YT_REMOTE_ENDPOINT') ?: '/php/yt/remote_server.php',
 ];
