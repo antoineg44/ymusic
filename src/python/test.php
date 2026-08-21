bd9afdf7fd3bf271cac972cebc8d1307b0738232
antoineg44 <antoine.godet@mailo.com>
2026-08-21 23:04:38 +0200

    test

--- src/python/test.php
+++ src/python/test.php
@@ -1,11 +1,25 @@
 <?php
 
 $ipv6 = "2a02:8424:894c:a901:211:32ff:fe99:630b";
+$url = 'http://[' . $ipv6 . ']:80/';
 
-$hostname = gethostbyaddr($ipv6);
+$ch = curl_init($url);
 
-if ($hostname === false || $hostname === $ipv6) {
-    echo "Aucun nom de domaine associé à cette IPv6.";
-} else {
-    echo "Nom d'hôte : " . $hostname;
+curl_setopt_array($ch, [
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_CONNECTTIMEOUT => 5,
+    CURLOPT_TIMEOUT => 15,
+]);
+
+$content = curl_exec($ch);
+
+if ($content === false) {
+    die('Erreur : ' . curl_error($ch));
 }
+
+$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+curl_close($ch);
+
+echo "HTTP $httpCode\n";
+echo $content;
