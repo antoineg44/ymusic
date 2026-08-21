9fafee752e49a3a277f75bd90c308c847decd4cc
antoineg44 <antoine.godet@mailo.com>
2026-08-21 16:20:53 +0200

    suite

--- src/python/test.php
+++ src/python/test.php
@@ -1,20 +1,20 @@
 <?php
 
-echo "<pre>";
+echo '<pre>';
 
-echo "=== libz ===\n";
-echo shell_exec("ldconfig -p 2>&1 | grep 'libz.so.1'");
+$commands = [
+    '/usr/bin/python3 -m pip install --user --dry-run yt-dlp 2>&1',
+    '/usr/bin/python3 -m pip install --user --dry-run "yt-dlp<2024.01.01" 2>&1',
+];
 
-echo "\n=== fichier système ===\n";
-echo shell_exec("ls -l /lib/x86_64-linux-gnu/libz.so.1 2>&1");
+foreach ($commands as $command) {
+    echo "========================================\n";
+    echo $command . "\n";
+    echo "========================================\n\n";
 
-echo "\n=== type ===\n";
-echo shell_exec("file /lib/x86_64-linux-gnu/libz.so.1 2>&1");
+    passthru($command, $return);
 
-echo "\n=== montage ===\n";
-echo shell_exec("mount 2>&1");
+    echo "\n\nCode retour : $return\n\n";
+}
 
-echo "\n=== environnement ===\n";
-echo shell_exec("uname -a 2>&1");
-
-echo "</pre>";
+echo '</pre>';
