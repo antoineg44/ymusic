06d051aaa0a7dba11b11e45b6886552b1bb7d271
antoineg44 <antoine.godet@mailo.com>
2026-08-21 16:22:30 +0200

    suite

--- src/python/test.php
+++ src/python/test.php
@@ -2,20 +2,10 @@
 
 echo '<pre>';
 
-$commands = [
-    '/usr/bin/python3 -m pip index versions yt-dlp 2>&1',
-    '/usr/bin/python3 -m pip install --user --no-deps "yt-dlp==2023.12.30" 2>&1',
-];
+$cmd = '/usr/bin/python3 -m pip install --user --no-deps "yt-dlp==2023.11.16" 2>&1';
 
-foreach ($commands as $command) {
+passthru($cmd, $return);
 
-    echo "========================================\n";
-    echo $command . "\n";
-    echo "========================================\n\n";
-
-    passthru($command, $return);
-
-    echo "\nCode retour : $return\n\n";
-}
+echo "\nCode retour : $return\n";
 
 echo '</pre>';
