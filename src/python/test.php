4f108384b8bb1087e2ffbfd73d7c9158ee5d7e0d
antoineg44 <antoine.godet@mailo.com>
2026-08-21 16:25:32 +0200

    suite

--- src/python/test.php
+++ src/python/test.php
@@ -1,12 +1,43 @@
 <?php
 
+$url = $_GET['url'] ?? '';
+
+if (!$url) {
+    ?>
+    <form method="get">
+        <input
+            type="url"
+            name="url"
+            placeholder="https://www.youtube.com/watch?v=..."
+            style="width:500px"
+            required
+        >
+        <button type="submit">Tester</button>
+    </form>
+    <?php
+    exit;
+}
+
+$ytdlp = '/home/partith/.local/bin/yt-dlp';
+
+$command =
+    escapeshellarg($ytdlp)
+    . ' --no-playlist'
+    . ' --dump-single-json'
+    . ' --skip-download'
+    . ' '
+    . escapeshellarg($url)
+    . ' 2>&1';
+
 echo '<pre>';
 
-passthru(
-    '/home/partith/.local/bin/yt-dlp --version 2>&1',
-    $return
-);
+$output = [];
+$return = 0;
 
-echo "\nCode retour : $return\n";
+exec($command, $output, $return);
+
+echo htmlspecialchars(implode("\n", $output));
+
+echo "\n\nCode retour : " . $return;
 
 echo '</pre>';
