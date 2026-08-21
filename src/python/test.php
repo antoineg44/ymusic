dcae4268f427c1b6235822e5d579acde5b071519
antoineg44 <antoine.godet@mailo.com>
2026-08-21 16:21:46 +0200

    suite

--- src/python/test.php
+++ src/python/test.php
@@ -3,18 +3,19 @@
 echo '<pre>';
 
 $commands = [
-    '/usr/bin/python3 -m pip install --user --dry-run yt-dlp 2>&1',
-    '/usr/bin/python3 -m pip install --user --dry-run "yt-dlp<2024.01.01" 2>&1',
+    '/usr/bin/python3 -m pip index versions yt-dlp 2>&1',
+    '/usr/bin/python3 -m pip install --user --no-deps "yt-dlp==2023.12.30" 2>&1',
 ];
 
 foreach ($commands as $command) {
+
     echo "========================================\n";
     echo $command . "\n";
     echo "========================================\n\n";
 
     passthru($command, $return);
 
-    echo "\n\nCode retour : $return\n\n";
+    echo "\nCode retour : $return\n\n";
 }
 
 echo '</pre>';
